7b33f3a0124899dce55ebc14f6d5cb3714179095
Shahadat Hosen <shahadat74238@gmail.com>
2023-11-07 15:52:06 +0600

    create and delate jwt token

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+var cookieParser = require('cookie-parser')
 const port = process.env.PORT || 3001;
 
 // ------------ Middleware --------------------
 app.use(express.json());
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cxghft2.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -17,8 +25,26 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+//  -------------- verify token -----------
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    // console.log(decoded);
+    req.user = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -26,41 +52,62 @@ async function run() {
     // await client.connect();
 
     const roomsCollection = client.db("fiveStar_hotel").collection("rooms");
-    const commentsCollection = client.db("fiveStar_hotel").collection("comments");
+    const commentsCollection = client
+      .db("fiveStar_hotel")
+      .collection("comments");
 
+    // ---------------- jwt token ----------------
+    app.post("/api/v1/token", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.SECRET, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    app.post('/api/v1/logout', async(req, res) => {
+      res.clearCookie('token', {maxAge: 0}).send({success: true});
+    })
+  
     // ------------- Get rooms ----------------------
-    app.get("/api/v1/rooms", async(req, res) => {
+    app.get("/api/v1/rooms", async (req, res) => {
       const result = await roomsCollection.find().toArray();
       res.send(result);
     });
 
-    app.get("/api/v1/rooms/:roomId", async(req, res) =>{
+    app.get("/api/v1/rooms/:roomId", async (req, res) => {
       const id = req.params.roomId;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await roomsCollection.findOne(query);
       res.send(result);
     });
 
     // --------------- Comments --------------------
 
-    app.post("/api/v1/comments", async(req, res) => {
+    app.post("/api/v1/comments", async (req, res) => {
       const comment = req.body;
       console.log(comment);
       const result = await commentsCollection.insertOne(comment);
-      res.send(result); 
+      res.send(result);
     });
 
-    app.get("/api/v1/comments", async(req, res) => {
+    app.get("/api/v1/comments", async (req, res) => {
       const id = req.query.roomId;
-      const query = {roomId: id};
+      const query = { roomId: id };
       const result = await commentsCollection.find(query).toArray();
       res.send(result);
     });
 
-
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -68,12 +115,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Five Star Hotel Backend is Running !')
-})
+app.get("/", (req, res) => {
+  res.send("Five Star Hotel Backend is Running !");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening port: ${port}`)
-})
+  console.log(`Example app listening port: ${port}`);
+});
